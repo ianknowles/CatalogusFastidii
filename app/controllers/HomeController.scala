7fc6b19b7165444ce2f41c2e6fa3f3710f3a00bf
ianknowles <ian@imknowles.co.uk>
2023-09-27 22:38:07 +0100

    commenting for home controller and index

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -6,18 +6,13 @@ import play.api.i18n.I18nSupport
 import play.api.mvc._
 
 /**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
+ * Controller for simple pages
  */
 @Singleton
 class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController with I18nSupport {
 
 	/**
-	 * Create an Action to render an HTML page.
-	 *
-	 * The configuration in the `routes` file means that this method
-	 * will be called when the application receives a `GET` request with
-	 * a path of `/`.
+	 * Render the index page from its HTML template
 	 */
 	def index() = Action { implicit request: Request[AnyContent] =>
 		Ok(views.html.index())
